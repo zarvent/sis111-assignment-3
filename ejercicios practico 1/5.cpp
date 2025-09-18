aab8cb66ec03561f493f01b4e752b5f688e6ac6f
zarvent <zarventt@gmail.com>
2025-09-18 12:46:58 -0400

    Refactor code formatting and improve comments for clarity

--- ejercicios practico 1/5.cpp
+++ ejercicios practico 1/5.cpp
@@ -1,18 +1,30 @@
+/*
+5. Leer n números por teclado y mostrar los números impares que se encuentran entre 150 y 550.
+
+*/
+
 #include <iostream>
 
 int main() {
-    int cantidadDeNumeros = 0;
-    int numero = 0;
+    // Declaración de variables
+    int cantidadDeNumeros = 0; // Almacena la cantidad total de números a  ngresar
+
+    int numero = 0;             // Almacena cada número ingresado por el usuario
+
 
     std::cout << "¿Cuántos números deseas ingresar? ";
     std::cin >> cantidadDeNumeros;
 
+
     std::cout << "\n--- Inicio de entrada (" << cantidadDeNumeros << " números) ---\n";
 
+
     for (int i = 0; i < cantidadDeNumeros; ++i) {
+        // Solicitar el número al usuario
         std::cout << "Número " << (i + 1) << " — ingresa valor: ";
         std::cin >> numero;
 
+        // Verificar si el número es impar y está en el rango [150, 550]
         if (numero >= 150 && numero <= 550 && numero % 2 != 0) {
             std::cout << "[✓] Número " << numero << "es impar y esta entre (150-550): "<< std::endl;
         } else{
@@ -20,6 +32,7 @@ int main() {
         }
     }
 
+    // Mensaje final
     std::cout << "\n--- Proceso finalizado. Gracias. ---\n";
 
     return 0;
